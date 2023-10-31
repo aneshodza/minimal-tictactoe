d15879ffff2e7789a56623752fb83ec601edf1a8
Anes Hodza <anes.hodza04@gmail.com>
2023-10-31 19:29:57 +0100

    Finish the script and output the field

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,64 @@
 use std::env;
 
 fn main() {
-    println!("Tictactoe encoded in only 26 bits");
+    println!("Tictactoe encoded in only 25 bits");
 
     let args: Vec<String> = env::args().collect();
     let input = &args[1].trim().parse::<u32>().unwrap();
 
-    let first_part = (input >> 21) & 0b1111;
-    let second_part = (input >> 17) & 0b1111;
-    let third_part = (input >> 14) & 0b111;
-    let fourth_part = (input >> 11) & 0b111;
-    let fifth_part = (input >> 8) & 0b111;
-    let sixth_part = (input >> 5) & 0b111;
-    let seventh_part = (input >> 3) & 0b11;
-    let eighth_part = (input >> 1) & 0b11;
-    let ninth_part = input & 0b1;
+    // Extract the parts as described
+    let parts = [
+        (input >> 21) & 0b1111,
+        (input >> 17) & 0b1111,
+        (input >> 14) & 0b111,
+        (input >> 11) & 0b111,
+        (input >> 8) & 0b111,
+        (input >> 5) & 0b111,
+        (input >> 3) & 0b11,
+        (input >> 1) & 0b11,
+        input & 0b1,
+    ];
 
-    println!("{:04b}", first_part);
-    println!("Second part (4 bits): {:04b}", second_part);
-    println!("Third part (3 bits): {:03b}", third_part);
-    println!("Fourth part (3 bits): {:03b}", fourth_part);
-    println!("Fifth part (3 bits): {:03b}", fifth_part);
-    println!("Sixth part (3 bits): {:03b}", sixth_part);
-    println!("Seventh part (2 bits): {:02b}", seventh_part);
-    println!("Eighth part (2 bits): {:02b}", eighth_part);
-    println!("Ninth part (1 bit): {:01b}", ninth_part);
+    // Initialize the Tic-Tac-Toe board
+    let mut board = [' '; 9];
+    let mut turn = 'X'; // Start with 'X'
+
+    // Function to place moves on the board
+    fn place_move(board: &mut [char], index: usize, turn: char) -> bool {
+        let mut skip_count = 0;
+        for cell in board.iter_mut() {
+            if *cell == ' ' {
+                if skip_count == index {
+                    *cell = turn;
+                    return true;
+                }
+                skip_count += 1;
+            }
+        }
+        false
+    }
+
+    // Iterate through the parts and place the moves
+    for (i, &part) in parts.iter().enumerate() {
+        // Convert part to 0-based index
+        let index = part as usize - 1;
+
+        // Try to place the move on the board
+        if place_move(&mut board, index, turn) {
+            // If successful, switch turns
+            turn = if turn == 'X' { 'O' } else { 'X' };
+        } else {
+            println!("Could not place move for part {}: {}", i + 1, part);
+        }
+    }
+
+    // Print the Tic-Tac-Toe board
+    for (i, &cell) in board.iter().enumerate() {
+        if i % 3 == 0 {
+            println!(); // Newline for new row
+        }
+        print!("{} ", cell);
+    }
+    println!(); // Newline at the end of the board
 }
+
